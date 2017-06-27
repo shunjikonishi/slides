3a4d9891ff11ffd389f03c249a4b52dde514f8fc
shunjikonishi <swandouble@gmail.com>
2017-06-28 00:10:19 +0900

    Rust

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,192 @@
-// Generated on 2014-05-23 using generator-revealjs 0.0.2
-'use strict';
-var LIVERELOAD_PORT = 8080;
-var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
-var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
-};
- 
-module.exports = function (grunt) {
-  // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
- 
+/* global module:false */
+module.exports = function(grunt) {
+  var port = grunt.option('port') || 8000;
+  var root = grunt.option('root') || '.';
+
+  if (!Array.isArray(root)) root = [root];
+
+  // Project configuration
   grunt.initConfig({
-    watch: {
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner:
+        '/*!\n' +
+        ' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
+        ' * http://lab.hakim.se/reveal-js\n' +
+        ' * MIT licensed\n' +
+        ' *\n' +
+        ' * Copyright (C) 2017 Hakim El Hattab, http://hakim.se\n' +
+        ' */'
+    },
+
+    qunit: {
+      files: [ 'test/*.html' ]
+    },
+
+    uglify: {
       options: {
-        nospawn: true,
-        livereload: LIVERELOAD_PORT
+        banner: '<%= meta.banner %>\n'
       },
-      livereload: {
+      build: {
+        src: 'js/reveal.js',
+        dest: 'js/reveal.min.js'
+      }
+    },
+
+    sass: {
+      core: {
+        files: {
+          'css/reveal.css': 'css/reveal.scss',
+        }
+      },
+      themes: {
         files: [
-          './{,*/}*.html',
-          './css/{,*/}*.css',
-          './{,*/}*.md',
+          {
+            expand: true,
+            cwd: 'css/theme/source',
+            src: ['*.sass', '*.scss'],
+            dest: 'css/theme',
+            ext: '.css'
+          }
         ]
       }
     },
-    connect: {
-      options: {
-        port: 9000,
-        base:'.',
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: '0.0.0.0'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [
-              lrSnippet,
-              mountFolder(connect, '.')
-            ];
-          }
+
+    autoprefixer: {
+      dist: {
+        src: 'css/reveal.css'
+      }
+    },
+
+    cssmin: {
+      compress: {
+        files: {
+          'css/reveal.min.css': [ 'css/reveal.css' ]
         }
       }
     },
-    open: {
+
+    jshint: {
+      options: {
+        curly: false,
+        eqeqeq: true,
+        immed: true,
+        esnext: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+          head: false,
+          module: false,
+          console: false,
+          unescape: false,
+          define: false,
+          exports: false
+        }
+      },
+      files: [ 'Gruntfile.js', 'js/reveal.js' ]
+    },
+
+    connect: {
       server: {
-        path: 'http://localhost:<%= connect.options.port %>/'
+        options: {
+          port: port,
+          base: root,
+          livereload: true,
+          open: true
+        }
+      },
+
+    },
+
+    zip: {
+      'reveal-js-presentation.zip': [
+        'index.html',
+        'css/**',
+        'js/**',
+        'lib/**',
+        'images/**',
+        'plugin/**',
+        '**.md'
+      ]
+    },
+
+    watch: {
+      js: {
+        files: [ 'Gruntfile.js', 'js/reveal.js' ],
+        tasks: 'js'
+      },
+      theme: {
+        files: [
+          'css/theme/source/*.sass',
+          'css/theme/source/*.scss',
+          'css/theme/template/*.sass',
+          'css/theme/template/*.scss'
+        ],
+        tasks: 'css-themes'
+      },
+      css: {
+        files: [ 'css/reveal.scss' ],
+        tasks: 'css-core'
+      },
+      html: {
+        files: root.map(path => path + '/*.html')
+      },
+      markdown: {
+        files: root.map(path => path + '/*.md')
+      },
+      options: {
+        livereload: true
       }
+    },
+
+    retire: {
+      js: ['js/reveal.js', 'lib/js/*.js', 'plugin/**/*.js'],
+      node: ['.'],
+      options: {}
     }
+
   });
- 
-  grunt.registerTask('default', ['connect:livereload', 'open', 'watch']);
+
+  // Dependencies
+  grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-sass' );
+  grunt.loadNpmTasks( 'grunt-contrib-connect' );
+  grunt.loadNpmTasks( 'grunt-autoprefixer' );
+  grunt.loadNpmTasks( 'grunt-zip' );
+  grunt.loadNpmTasks( 'grunt-retire' );
+
+  // Default task
+  grunt.registerTask( 'default', [ 'css', 'js' ] );
+
+  // JS task
+  grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+
+  // Theme CSS
+  grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
+
+  // Core framework CSS
+  grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
+
+  // All CSS
+  grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
+
+  // Package presentation to archive
+  grunt.registerTask( 'package', [ 'default', 'zip' ] );
+
+  // Serve presentation locally
+  grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
+
+  // Run tests
+  grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
+
 };
--- rust/content.md
+++ rust/content.md
@@ -1,6 +1,7 @@
-# はじめてのRust
+# はじめての Rust
 ---
 ### Who are you
+![social](images/social_icon.jpg)
 - @shunjikonishi
 - 株式会社 Givery エンジニア
   - Services: CODEPREP, code-check
@@ -10,26 +11,64 @@
 
 ---
 ### CODEPREP
-- プログラミング学習サイト
-  
+- https://codeprep.jp
+![ブック一覧](images/codeprep1.png)
+
+-->
+![リーダー](images/codeprep3.png)
+
 ---
 ### RustのBook
 - これまでに3冊のRust Bookをリリース
+![Rust Books](images/rust_books.png)
 
 ---
-### 著者
-- Brian Schwind
+### 書いた人
+![Brian](images/bschwind.png)
 
 ---
 ### はじめてのRust
 
-# 俺がな！
+# 翻訳
+
+---
+### Rustブックの中身
+![Rust1](images/rust_page1.png)
 
 ---
 ### アメリカンジョーク
-- 絵文字!
+![Rust2](images/rust_joke.png)
 
+- "🥚"を"🍚"に、"🥓"を"🐟"に置換する
 
 ---
-### まとめ
+### ブックリリース履歴
+- **毎週水曜日に2〜3冊をリリース**
+- 本日リリース
+  - Rubyのクラスを理解する
+  - HTMLとCSS3で作るパンくず
+- Rustブック
+  - 5/3  初めてのRust(前編)
+  - 5/24 初めてのRust(後編)
+  - 6/7  Rustの特徴を理解する 所有権編
+  - <span style="color:red;">次の予定はない</span> <!-- .element: class="fragment" data-fragment-index="1" -->
+---
+### 何故なら読まれてないから。。。
+- 一番人気のブック(HTML5)の50分の1以下
+- Scalaも似たようなもの。。。
+  - (今週はScalaの方がちょっと人気)
 
+---
+### 大事なこと
+<ul>
+  <li>今後Rustブックを増やすためには</li>
+  <ul>
+    <li class="fragment" data-fragment-index="1">ReadCountを増やす</li>
+    <li class="fragment" data-fragment-index="2">Twitterで~~圧力をかける~~拡散する</li>
+  </ul>
+  <li class="fragment" data-fragment-index="3">**なんなら書いてくれる人募集**</li>
+  <li class="fragment" data-fragment-index="4"><span style="color:red;">Scalaもよろしくお願いします。</span></li>
+</ul>
+
+-->
+## 無料で読めるのは<br>今だけです！
